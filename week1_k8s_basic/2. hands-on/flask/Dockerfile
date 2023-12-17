7ca914d6416f3a2d1672f7fb9bf2c64398b913d8
Jonghyeok K <jka236@sfu.ca>
2023-12-17 15:35:15 -0800

    file organized

---
+++ week1_k8s_basic/2. hands-on/flask/Dockerfile
@@ -0,0 +1,22 @@
+# Use an official Python runtime as a parent image
+FROM python:slim
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+ADD . /app
+
+# Install any needed packages specified in requirements.txt
+
+RUN apt-get update && apt-get install -y libpq-dev python3-dev build-essential gcc
+RUN pip install -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
---
+++ week1_k8s_basic/2. hands-on/flask/app.py
@@ -0,0 +1,151 @@
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
+import os
+import socket
+
+app = Flask(__name__)
+
+DATABASE = "todo"
+USER = os.environ.get('POSTGRES_USER')
+PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+HOST = 'postgres'
+PORT = 5432
+
+@app.route("/")
+def hello():
+    html = """Hello {name}!
+    Hostname: {hostname}"""
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
+@app.route('/init')
+def init():
+    # Connect to the database
+    conn = psycopg2.connect(database=DATABASE, user=USER,
+                            password=PASSWORD, host=HOST, port=PORT)
+
+    # create a cursor
+    cur = conn.cursor()
+
+    # if you already have any table or not id doesnt matter this
+    # will create a products table for you.
+    cur.execute(
+        '''CREATE TABLE IF NOT EXISTS products (id serial \
+        PRIMARY KEY, name varchar(100), price float);''')
+
+    # Insert some data into the table
+    cur.execute(
+        '''INSERT INTO products (name, price) VALUES \
+        ('Apple', 1.99), ('Orange', 0.99), ('Banana', 0.59);''')
+
+    # commit the changes
+    conn.commit()
+
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+    return {'status': 'success'}
+
+@app.route('/get')
+def index():
+	# Connect to the database
+	conn = psycopg2.connect(database=DATABASE,
+							user=USER,
+							password=PASSWORD,
+							host=HOST, port=PORT)
+
+	# create a cursor
+	cur = conn.cursor()
+
+	# Select all products from the table
+	cur.execute('''SELECT * FROM products''')
+
+	# Fetch the data
+	data = cur.fetchall()
+
+	# close the cursor and connection
+	cur.close()
+	conn.close()
+
+	return render_template('index.html', data=data)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+	conn = psycopg2.connect(database=DATABASE,
+							user=USER,
+							password=PASSWORD,
+							host=HOST, port=PORT)
+
+	cur = conn.cursor()
+
+	# Get the data from the form
+	name = request.form['name']
+	price = request.form['price']
+
+	# Insert the data into the table
+	cur.execute(
+		'''INSERT INTO products \
+		(name, price) VALUES (%s, %s)''',
+		(name, price))
+
+	# commit the changes
+	conn.commit()
+
+	# close the cursor and connection
+	cur.close()
+	conn.close()
+
+	return redirect(url_for('index'))
+
+
+@app.route('/update', methods=['POST'])
+def update():
+	conn = psycopg2.connect(database=DATABASE,
+							user=USER,
+							password=PASSWORD,
+							host=HOST, port=PORT)
+
+	cur = conn.cursor()
+
+	# Get the data from the form
+	name = request.form['name']
+	price = request.form['price']
+	id = request.form['id']
+
+	# Update the data in the table
+	cur.execute(
+		'''UPDATE products SET name=%s,\
+		price=%s WHERE id=%s''', (name, price, id))
+
+	# commit the changes
+	conn.commit()
+	return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+	conn = psycopg2.connect(database=DATABASE,
+							user=USER,
+							password=PASSWORD,
+							host=HOST, port=PORT)
+
+	cur = conn.cursor()
+ 
+	# Get the data from the form
+	id = request.form['id']
+
+	# Delete the data from the table
+	cur.execute('''DELETE FROM products WHERE id=%s''', (id,))
+
+	# commit the changes
+	conn.commit()
+
+	# close the cursor and connection
+	cur.close()
+	conn.close()
+
+	return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
